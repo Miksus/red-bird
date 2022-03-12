0e4c36b863aacd5817a3a864d10b375bf4c37346
Mikael Koli <koli.mikael@gmail.com>
2022-03-12 12:32:24 +0200

    pkg: add setup files

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import versioneer
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="redrepo",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Mikael Koli",
+    author_email="koli.mikael@gmail.com",
+    url="https://github.com/Miksus/red-repo.git",
+    packages=find_packages(),
+    description="Repository Patterns for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+
+        "Intended Audience :: Developers",
+     ],
+     include_package_data=True, # for MANIFEST.in
+     python_requires='>=3.6.0',
+
+    install_requires = [],
+)
