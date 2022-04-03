0cdf1774db9fa0e1a4cbf89197a2f56ca5744493
Mikael Koli <koli.mikael@gmail.com>
2022-04-03 11:38:42 +0300

    add: add url params to rest repo

--- redbase/repos/rest.py
+++ redbase/repos/rest.py
@@ -13,7 +13,8 @@ class RESTResult(BaseResult):
 
     def query(self):
         url = self.repo.get_url(self.query_)
-        output = self.repo._request("GET", url).json()
+        page = self.repo._request("GET", url)
+        output = page.json()
         if isinstance(output, (list, tuple, set)):
             for item in output:
                 yield self.repo.parse_item(item)
@@ -37,11 +38,14 @@ class RESTRepo(BaseRepo):
 
     cls_result = RESTResult
 
-    def __init__(self, model:BaseModel, url, id_field=None):
+    def __init__(self, model:BaseModel, url, id_field=None, headers:dict=None, url_params:dict=None):
         self.model = model
         self.url = url
         self.id_field = id_field
         self.session = requests.Session()
+
+        self.headers = headers
+        self.url_params = {} if url_params is None else url_params
 
     def insert(self, item):
         json = self.item_to_dict(item)
@@ -57,16 +61,19 @@ class RESTRepo(BaseRepo):
         self.filter_by(**qry).replace(**values)
 
     def _request(self, *args, **kwargs):
-        return self.session.request(
-            *args, **kwargs
+        page = self.session.request(
+            *args, **kwargs, headers=self.headers
         )
+        return page
 
     def get_url(self, query=None):
-        query = {} if query is None else query
+        url_params = self.url_params.copy()
+        if query is not None:
+            url_params.update(query)
         id = query.pop(self.id_field) if self.id_field in query else None
 
         url_base = self.url
-        url_params = urlparse.urlencode(query)
+        url_params = urlparse.urlencode(url_params) # Turn {"param": "value"} --> "param=value"
 
         if id is None:
             id = ""
@@ -75,4 +82,8 @@ class RESTRepo(BaseRepo):
         if url_params:
             url_params = "?" + url_params
 
+        # URL should look like "www.example.com/api/items/{id}?{param}={value}"
+        # or "www.example.com/api/items/{id}"
+        # or "www.example.com/api/items?{param}={value}"
+        # or "www.example.com/api/items"
         return f"{url_base}{id}{url_params}"
