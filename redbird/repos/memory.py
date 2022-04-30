d69bcd1d78b187f40f44400fa0b034ea3655dac3
Mikael Koli <koli.mikael@gmail.com>
2022-04-30 10:23:37 +0300

    upd: now MemoryRepo uses template repo

--- redbird/repos/memory.py
+++ redbird/repos/memory.py
@@ -4,49 +4,27 @@ from typing import Dict, List
 
 from pydantic import BaseModel
 from redbird import BaseRepo, BaseResult
+from redbird.templates import TemplateRepo
 from redbird.exc import KeyFoundError
 from redbird.oper import Operation
 from redbird.dummy import DummySession
 
-class MemoryResult(BaseResult):
 
-    """Filter object of MemoryRepo"""
+class QueryMatcher:
 
-    repo: 'MemoryRepo'
+    def __init__(self, query, value_getter):
+        self.query = query
+        self.value_getter = value_getter
 
-    def query_data(self):
-        col = self.repo.collection
-        for data in col:
-            if self._match(data):
-                yield data
-
-    def update(self, **kwargs):
-        col = self.repo.collection
-        for item in col:
-            if self._match(item):
-                for key, val in kwargs.items():
-                    self.repo.set_field_value(item, key, val)
-
-    def delete(self, **kwargs):
-        cont = self.repo.collection
-        self.repo.collection = [
-            item 
-            for item in cont 
-            if not self._match(item)
-        ]
-
-    def format_query(self, query:dict):
-        return query
-
-    def _match(self, item):
+    def __contains__(self, item):
         "Match whether item fulfills the query"
-        get_value = self.repo.get_field_value
+        get_value = self.value_getter
         return all(
             val.evaluate(get_value(item, key)) if isinstance(val, Operation) else get_value(item, key) == val
-            for key, val in self.query_.items()
+            for key, val in self.query.items()
         )
 
-class MemoryRepo(BaseRepo):
+class MemoryRepo(TemplateRepo):
     """Memory repository
 
     This is a repository which operates in memory.
@@ -59,7 +37,7 @@ class MemoryRepo(BaseRepo):
     collection : list
         The collection.
     """
-    cls_result = MemoryResult
+    #cls_result = MemoryResult
     collection: List[BaseModel]
 
     def __init__(self, *args, collection=None, **kwargs):
@@ -76,3 +54,27 @@ class MemoryRepo(BaseRepo):
 
     def item_to_data(self, item):
         return item
+
+    def query_read(self, query):
+        query = QueryMatcher(query, value_getter=self.get_field_value)
+        col = self.collection
+        for data in col:
+            if data in query:
+                yield data
+
+    def query_update(self, query:dict, values:dict):
+        query = QueryMatcher(query, value_getter=self.get_field_value)
+        col = self.collection
+        for item in col:
+            if item in query:
+                for key, val in values.items():
+                    self.set_field_value(item, key, val)
+
+    def query_delete(self, query:dict):
+        query = QueryMatcher(query, value_getter=self.get_field_value)
+        cont = self.collection
+        self.collection = [
+            item 
+            for item in cont 
+            if not item in query
+        ]
