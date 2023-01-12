a69537b45c70efcd936f9dd5e4261f359963a4e8
Mikael Koli <koli.mikael@gmail.com>
2023-01-12 23:29:53 +0200

    test: more select tests

--- redbird/test/sql/operations/test_select.py
+++ redbird/test/sql/operations/test_select.py
@@ -12,9 +12,18 @@ def test_select_all(engine):
         {'id': 'c', 'name': 'James', 'birth_date': date(2020, 1, 1), 'score': 300},
     ] == list(results)
 
-def test_select_string_parametize(engine):
-    qry = "select * from populated where name = :name"
-    results = select(qry, parameters={"name": "Jack"}, bind=engine)
+@pytest.mark.parametrize("how", [
+    "raw string",
+    "expression"
+])
+def test_select_parametize(engine, how):
+    sqlalchemy = pytest.importorskip("sqlalchemy")
+    if how == "raw string":
+        qry = "select * from populated where name = :name"
+    elif how == "expression":
+        qry = sqlalchemy.text("select * from populated where name = :name")
+        
+    results = select(qry, bind=engine, parameters={"name": "Jack"})
     assert [
         {'id': 'a', 'name': 'Jack', 'birth_date': '2000-01-01', 'score': 100},
         #{'id': 'b', 'name': 'John', 'birth_date': '1990-01-01', 'score': 200},
@@ -89,17 +98,30 @@ def test_select_range(engine, how):
     ] == list(results)
 
 @pytest.mark.parametrize("how", [
-    "raw string",
+    "raw string", "statement"
 ])
 def test_select_without_table(engine, how):
+    import sqlalchemy
     if how == "raw string":
         qry = "SELECT * FROM populated where name in ('Jack', 'John')"
+    elif how == "statement":
+        tbl = Table("populated", bind=engine).object
+        qry = sqlalchemy.select(tbl).where(tbl.columns['name'].in_(["Jack", "John"]))
+
     results = select(qry, bind=engine)
-    assert [
-        {'id': 'a', 'name': 'Jack', 'birth_date': '2000-01-01', 'score': 100},
-        {'id': 'b', 'name': 'John', 'birth_date': '1990-01-01', 'score': 200},
-        #{'id': 'c', 'name': 'James', 'birth_date': '2020-01-01', 'score': 300},
-    ] == list(results)
+    
+    if how == "statement":
+        assert [
+            {'id': 'a', 'name': 'Jack', 'birth_date': date(2000, 1, 1), 'score': 100},
+            {'id': 'b', 'name': 'John', 'birth_date': date(1990, 1, 1), 'score': 200},
+            #{'id': 'c', 'name': 'James', 'birth_date': '2020-01-01', 'score': 300},
+        ] == list(results)
+    else:
+        assert [
+            {'id': 'a', 'name': 'Jack', 'birth_date': '2000-01-01', 'score': 100},
+            {'id': 'b', 'name': 'John', 'birth_date': '1990-01-01', 'score': 200},
+            #{'id': 'c', 'name': 'James', 'birth_date': '2020-01-01', 'score': 300},
+        ] == list(results)
 
 def test_select_dict(engine):
     results = select({"name": "James", "birth_date": date(2020, 1, 1)}, table="populated", bind=engine)
