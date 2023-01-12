b8d3c19fc5b2ec1e045dd1f3a1ee4e1aa7653bab
Mikael Koli <koli.mikael@gmail.com>
2023-01-12 23:44:08 +0200

    add: SQLExpressionRepo

--- redbird/repos/__init__.py
+++ redbird/repos/__init__.py
@@ -1,7 +1,7 @@
 
 from .memory import MemoryRepo
 from .mongo import MongoRepo
-from .sqlalchemy import SQLRepo
+from .sqlalchemy import SQLRepo, SQLExpressionRepo
 from .rest import RESTRepo
 from .csv import CSVFileRepo
 from .json import JSONDirectoryRepo
